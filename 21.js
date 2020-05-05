aaf0e65164e66f5831f1b4d2392085b72bce5ee3
iglarracoechea <46580285+iglarracoechea@users.noreply.github.com>
2020-05-05 01:14:10 -0300

    Update 21.js

--- 21.js
+++ 21.js
@@ -1,9 +1,11 @@
+
 'use strict'
 //rograma una función que dado un array numérico devuelve otro array 
 //con los números elevados al cuadrado, pe. mi_funcion([1, 4, 5]) devolverá [1, 16, 25].
 
 
 const array = (numeros=[])=>{
+	
 	let cont = 0
 	let elemento = ''
 	
@@ -14,9 +16,32 @@ const array = (numeros=[])=>{
 	}while(cont < 6)
 
 
-	let newArray = numeros.map((elemento) => elemento*elemento)
+	let newArray = numeros.map(elemento => elemento * elemento)
 	return console.log(newArray)
 
 }
 
-array()
+//array()
+//arreglos, date, objetos con instanceof
+//otra forma de hacerlo
+
+const devolverCuadrados = (arreglo = undefined) => {
+	if(arreglo === undefined) return console.warn ('No ingreso arreglo')
+	if(!(arreglo instanceof Array)) return console.log('El valor expresado no es un array')
+	if(arreglo.length === 0) return console.log('El arreglo esta vacio')
+	
+
+	for(let numeros of arreglo){
+		if(typeof numeros !== 'number') return console.log(`El valor ${numeros} no es un numero`)
+	}
+
+	const newArray = arreglo.map(numero => numero * numero)
+	return console.log('Arreglo original: '+ arreglo + '. Arreglo nuevo: ' + newArray)
+}
+
+devolverCuadrados()
+devolverCuadrados(true)
+devolverCuadrados({})
+devolverCuadrados([])
+devolverCuadrados([1,'3'])
+devolverCuadrados([1,3,5])
