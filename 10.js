ce5c9f9229f74448bf0704aebc89d672c750b77e
iglarracoechea <46580285+iglarracoechea@users.noreply.github.com>
2020-06-06 03:05:48 -0300

    Update 10.js

--- 10.js
+++ 10.js
@@ -2,3 +2,5 @@
 const aleatorio = (max,min) => {return Math.round(Math.random()*(501-600) + 501)}  //para generar numero al azar
 
 console.log(aleatorio())
+
+console.log('Aleatorio '+ Math.random()*10000)
